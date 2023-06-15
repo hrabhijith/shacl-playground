8c92a03a0aee92918dbfa02e349db1eff3880c94
Abhijith H R <abhijith.hr@ib-systems.org>
2023-06-15 18:12:27 +0200

    first commit

--- chat.py
+++ chat.py
@@ -4,6 +4,7 @@ from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.embeddings import HuggingFaceEmbeddings
+import os
 
 # add the path to the CV as a PDF
 loader = TextLoader('data.txt')
@@ -18,8 +19,9 @@ index = VectorstoreIndexCreator(embedding= HuggingFaceEmbeddings()).from_loaders
 # # Verbose is required to pass to the callback manager
 # llm = GPT4All(model=local_path, callbacks=callbacks, verbose=True, backend='gptj')
 
+question = str(input("Please enter your question: "))
 # perform similarity search and retrieve the context from our documents
-results = index.vectorstore.similarity_search("Full shacl shape for iff namespace", k=2)
+results = index.vectorstore.similarity_search(question, k=2)
 # join all context information (top 4) into one string 
 context = "\n".join([document.page_content for document in results])
 # print(f"Retrieving information related to your question...")
@@ -36,5 +38,12 @@ prompt = PromptTemplate(template=template, input_variables=["context", "question
 llm = OpenAI(temperature=0)
 llm_chain = LLMChain(prompt=prompt, llm=llm)
 # Print the result
-print("Processing the information with gpt4all...\n")
-print(llm_chain.run("write a shacl ttl file For the cutting process at CNC cutting machines a defined pressure as well as a given flow rate of gas (or different gases, depending on the process) has to be kept. To ensure that these parameters are met, the gas sampling point connected to the CNC cutting system is to be equipped with appropriate sensor technology and the values are to be documented. Requires specific window 10 -12 bar at inlet. Input pressure is measured, output pressure is set and output. use the the given context and namespace to write the shacl file."))
+print("Processing the information with OpenAI...\n")
+
+# E.g, write a shacl ttl file For the cutting process at CNC cutting machines a defined pressure 
+# as well as a given flow rate of gas (or different gases, 
+# depending on the process) has to be kept. To ensure that these parameters are met, the gas sampling
+# point connected to the CNC cutting system is to be equipped with appropriate sensor technology and 
+# the values are to be documented. Requires specific window 10 -12 bar at inlet. Input pressure 
+# is measured, output pressure is set and output. use the the given context and namespace to write the shacl file.
+print(llm_chain.run(question))
